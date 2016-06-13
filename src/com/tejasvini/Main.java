9f8138227ab2fcfd5b6c853df8e0a12bc70bf402
atulksh <atulksh@hotmail.com>
2016-06-12 17:14:57 -0700

    First GitHub commit.

---
+++ src/com/tejasvini/Main.java
@@ -0,0 +1,12 @@
+package com.tejasvini;
+
+/**
+ * Created by Atul Kshatriya on 6/12/2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
